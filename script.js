a26f4493ef0de7829a996c3f3fc78c706973be2d
konrad00 <100691793+konrad0000@users.noreply.github.com>
2022-05-10 11:28:36 +0000

    5_2

--- script.js
+++ script.js
@@ -33,7 +33,20 @@
   
 
   cw2.addEventListener("click", function () {
-    
+    answer.innerHTML = "Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML = "";
+      for (var i = 0; i <data.length; i++){
+        var div = document.createElement("div");
+        div.innerHTML= '<strong>UserID: ' + data[i].userId + '</strong> <strong>id: ' + data[i].id + '</strong> <br> <strong>title: </strong>' + data[i].title + '<br> <strong>body:</strong> '+ data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
   })
 
   cw3.addEventListener("click", function () {
